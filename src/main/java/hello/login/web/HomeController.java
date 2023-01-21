51381dcc0780a32e79d3d47754fdf0cc81e94ea0
chaelynjang <chaelynjang@lotte.net>
2023-01-21 21:47:37 +0900

    직접 만든 세션 적용해보기

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
@@ -18,13 +20,14 @@ import javax.servlet.http.HttpServletResponse;
 @RequiredArgsConstructor
 public class HomeController {
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
     //@GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     // 로그인 됐을 때도 처리해주는 메소드 - 로그인 여부에 따라 보여주는 화면이 다름
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if(memberId == null) {
@@ -40,15 +43,20 @@ public class HomeController {
         return "loginHome";
     }
 
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
-        expireCookie(response, "memberId");
-        return "redirect:/";
+    @GetMapping("/")
+    // 로그인 됐을 때도 처리해주는 메소드 - 로그인 여부에 따라 보여주는 화면이 다름
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+        // 세션 관리자에 저장된 회원정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        // 로그인한 사용자
+
+        if(member == null) { // DB 에서 찾은 멤버가 없을 경우
+            return "home";
+        }
+        model.addAttribute("member", member);
+
+        return "loginHome";
     }
 
-    private void expireCookie(HttpServletResponse response, String cookieName) {
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-    }
 }
