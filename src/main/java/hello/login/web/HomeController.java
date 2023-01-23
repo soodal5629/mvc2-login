ff026a9ce05594f8a1c8fd0957f8cd4d37be37e6
chaelynjang <chaelynjang@lotte.net>
2023-01-23 11:58:31 +0900

    @SessionAttribute 적용 및 처음 로그인시 url에 sessionid 포함되지 않도록 설정

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -60,7 +61,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     // 로그인 됐을 때도 처리해주는 메소드 - 로그인 여부에 따라 보여주는 화면이 다름
     public String homeLoginV3(HttpServletRequest request, Model model) {
         // 화면에 처음 진입한 사람들한테도 세션이 생성되면 안되기 떄문에 false
@@ -71,6 +72,20 @@ public class HomeController {
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
 
+        // 세션에 회원 데이터가 없으면 home
+        if(loginMember == null) { // DB 에서 찾은 멤버가 없을 경우
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    // 로그인 됐을 때도 처리해주는 메소드 - 로그인 여부에 따라 보여주는 화면이 다름
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember
+            , Model model) {
         // 세션에 회원 데이터가 없으면 home
         if(loginMember == null) { // DB 에서 찾은 멤버가 없을 경우
             return "home";
